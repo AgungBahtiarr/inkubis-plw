d67b1f95a69ff3d2dd27612407a67068fd356ef8
AgungBahtiarr <agungbahtiar050@gmail.com>
2024-07-13 20:09:08 +0700

    add decap oauth

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,42 +10,30 @@ import vercel from "@astrojs/vercel/serverless";
 import netlify from "@astrojs/netlify";
 import remarkCollapse from "remark-collapse";
 
+import decapCmsOauth from "astro-decap-cms-oauth";
+
 // https://astro.build/config
 export default defineConfig({
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
-  integrations: [
-    react(),
-    sitemap(),
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    AutoImport({
-      imports: [
-        "@shortcodes/Button",
-      ],
-    }),
-    mdx(),
-  ],
+  integrations: [react(), sitemap(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), AutoImport({
+    imports: ["@shortcodes/Button"]
+  }), mdx(), decapCmsOauth()],
   markdown: {
-    remarkPlugins: [
-      remarkToc,
-      [
-        remarkCollapse,
-        {
-          test: "Table of contents",
-        },
-      ],
-    ],
+    remarkPlugins: [remarkToc, [remarkCollapse, {
+      test: "Table of contents"
+    }]],
     shikiConfig: {
       theme: "one-dark-pro",
-      wrap: true,
+      wrap: true
     },
-    extendDefaultPlugins: true,
+    extendDefaultPlugins: true
   },
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
